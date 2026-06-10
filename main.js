3176a06c6b4d8839fcdf074cd53eba07bf399b52
Mattytee18 <leftymattturner@aol.com>
2026-06-09 23:16:11 -0400

    Fix main.js location

--- main.js
+++ main.js
@@ -5,10 +5,19 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1100,
     height: 850,
-    webPreferences: { nodeIntegration: false }
+    icon: path.join(__dirname, 'electron', 'icon.ico'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
   })
-  win.loadFile(path.join(__dirname, '../dist/index.html'))
+  win.loadFile(path.join(__dirname, 'dist', 'index.html'))
 }
 
 app.whenReady().then(createWindow)
-app.on('window-all-closed', () => app.quit())
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
